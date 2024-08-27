6a0ce03c21cf2077edde22441202e3041d6824b7
Muhammad <ma.khan6971@gmail.com>
2024-08-27 19:34:10 +0100

    added svgs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import React from "react";
 import { navItems } from "./data";
+import Grid from "@/components/Grid";
 
 const page = () => {
   return (
@@ -9,6 +10,7 @@ const page = () => {
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Hero />
+        <Grid />
       </div>
     </main>
   );

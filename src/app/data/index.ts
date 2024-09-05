3ffe895afdb36df7c56576aa15d4fdfc90a6add7
Muhammad <ma.khan6971@gmail.com>
2024-09-05 15:31:17 +0100

    fixed bug

--- src/app/data/index.ts
+++ src/app/data/index.ts
@@ -48,7 +48,7 @@ export const navItems = [
   
     {
       id: 5,
-      title: "Currently building a JS Animation library",
+      title: "Currently building a Django Nextjs application",
       description: "The Inside Scoop",
       className: "md:col-span-3 md:row-span-2",
       imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
